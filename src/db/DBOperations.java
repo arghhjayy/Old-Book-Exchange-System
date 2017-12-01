dd91bc3b62411963c665f7e16606c67974bbd6d3
Rupanshu Janbandhu <arghhjayy@gmail.com>
2017-12-01 17:05:37 +0530

    Add some database operations: create a user and check if a user exists

--- src/db/DBOperations.java
+++ src/db/DBOperations.java
@@ -1,8 +1,8 @@
 package db;
 
 import static data.SQLData.*;
+import static encryption.MD5Encryption.md5;
 import java.sql.*;
-
 
 public class DBOperations {
 	static{
@@ -16,7 +16,47 @@ public class DBOperations {
 	
 	public static void create_user(String username, String password){
 		String sql = "INSERT INTO APPUSER(USERNAME, PASSWORD)"+
-						"VALUES(" + username + ", " + password + ")";
+						" VALUES('" + username + "', '" + md5(password) + "')";
+		
+		System.out.println(sql);
+		
+		Connection con = null;
+		Statement stmt = null;
+		
+		try {
+			con = DriverManager.getConnection(databaseLocation,
+							SQLServerUsername, SQLServerPassword);
+			stmt = con.createStatement();
+			stmt.executeUpdate(sql);
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally{
+			try{
+				if(stmt != null)
+					stmt.close();
+			} catch(SQLException ex){
+				ex.printStackTrace();
+			}
+			
+			try {
+				if(con != null)
+					con.close();
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	public static void create_user(String username, String password, 
+									String firstname, String lastname)
+	{
+		String sql = "INSERT INTO APPUSER"+
+					" VALUES('" + username + "', " +
+							"'" + firstname + "', " +
+							"'" + lastname + "', " +
+							"'" + md5(password) + "')";
+		
+		System.out.println(sql);
 		
 		Connection con = null;
 		Statement stmt = null;
@@ -47,7 +87,8 @@ public class DBOperations {
 	
 	public static boolean hasUser(String username){
 		try {
-			Connection con = DriverManager.getConnection(databaseLocation, SQLServerUsername, SQLServerPassword);
+			Connection con = DriverManager.getConnection(databaseLocation,
+									SQLServerUsername, SQLServerPassword);
 			
 			Statement stmt = con.createStatement();
 			
@@ -63,8 +104,7 @@ public class DBOperations {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("Fuck");
-		create_user("sivaji", "theboss");
-		System.out.println(hasUser("sivaji"));
+		create_user("posty", "postymalone");
+		System.out.println(hasUser("posty"));
 	}
 }
